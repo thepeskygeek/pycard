dc372c3212470b2ee6931ee435bc715d79615b12
Andrew Steinborn <andrew@arandomblog.co.cc>
2010-01-09 00:00:11 -0500

    implement phrases like in gen_card

--- gen_hash.py
+++ gen_hash.py
@@ -7,8 +7,9 @@ if __name__ == '__main__':
     n = raw_input('Name: ')
     e = raw_input('Email: ')
     c = raw_input('Signer Server URL: ')
-    random.seed(hash(random.getrandbits(20))+hash(os.urandom(20))+6+hash(n))
-    i = random.randrange(1, 999999999)
+    h = raw_input('(Optional) Phrase: ')
+    random.seed(hash(random.getrandbits(20))+6+hash(n)+hash(h)+random.random())
+    i = random.randrange(1, 99999999999)
     print "Unqiue ID:"
     print i
     s = n + e
